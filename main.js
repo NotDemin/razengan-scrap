c890f015aa3bcee733b012268d171e192c3cc618
nathan <nathan@ip-soft.fr>
2024-10-18 11:20:45 +0200

    utils

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
 import axios from 'axios';
 import 'dotenv/config'
 import querystring from 'querystring';
+import { display_errors, validate_url, check_env_vars } from './utils.js';
 
 const MAIN_URL_EXTRACT = process.argv[2]
 const OUTPUT_JSON = process.argv[3]?.toLowerCase() === "true" ? true : false
-
-const display_errors = (msg_error) => {
-    if (OUTPUT_JSON) {
-        console.log(JSON.stringify({error: msg_error}))
-    } else {
-        console.error(msg_error)
-    }
-}
-
-
-const check_env_vars = () => {
-    if (!process.env.RAZENGAN_LOG || !process.env.RAZENGAN_PWD || !process.env.RAZENGAN_URL || !process.env.COOKIE_JSP_1 || !process.env.COOKIE_JSP_2) {
-        const missing_vars = []
-        const var_to_check = ["RAZENGAN_LOG", "RAZENGAN_PWD", "RAZENGAN_URL", "COOKIE_JSP_1", "COOKIE_JSP_2"]
-
-        var_to_check.forEach(var_name => {
-            if (!process.env[var_name]) {
-                missing_vars.push(var_name)
-            }
-        })
-
-        display_errors("Variables d'environnement manquantes: " + missing_vars.join(", "))
-        process.exit(1)
-    }
-}
-
-const validate_url = (url) => {
-    const regex_url = /^https:\/\/razengan\.club\/\?page_id=\d+$/g
-    return regex_url.test(url)
-}
 
 const get_anime_name = (html) => {
     const title_start = html.indexOf("Protégé : Liens")
@@ -87,7 +58,9 @@ const get_password = async (token) => {
         );
     } catch (e) {
         if (e.status === 404) {
-            display_errors("Page non trouvée")
+            display_errors("Page non trouvée", OUTPUT_JSON)
+        } else {
+            display_errors("Erreur HTTP: " + e.status, OUTPUT_JSON)
         }
         process.exit(1)
     }
@@ -110,7 +83,7 @@ const get_password = async (token) => {
     if (start_extract_pwd !== -1 && end_extract_pwd !== -1) {
         password_clean = final_str.substring(start_extract_pwd + start.length, end_extract_pwd)
     } else {
-        display_errors("Les caractères spécifiés ne sont pas présents. Vérifiez qu'il s'agit bien d'une page a mot de passe")
+        display_errors("Les caractères spécifiés ne sont pas présents. Vérifiez qu'il s'agit bien d'une page a mot de passe", OUTPUT_JSON)
         process.exit(1)
     }
 
@@ -139,7 +112,9 @@ const get_1fichier_page = async (password, token) => {
         );
     } catch (e) {
         if (e.status === 404) {
-            display_errors("Page non trouvée")
+            display_errors("Page non trouvée", OUTPUT_JSON)
+        } else {
+            display_errors("Erreur HTTP: " + e.status, OUTPUT_JSON)
         }
         process.exit(1)
     }
@@ -165,13 +140,15 @@ const get_1fichier_page = async (password, token) => {
         );
     } catch (e) {
         if (e.status === 404) {
-            display_errors("Page non trouvée")
+            display_errors("Page non trouvée", OUTPUT_JSON)
+        } else {
+            display_errors("Erreur HTTP: " + e.status, OUTPUT_JSON)
         }
         process.exit(1)
     }
 
     if (!links_after_pwd.data.includes("1fichier")) {
-        display_errors("Aucun lien 1fichier trouvé")
+        display_errors("Aucun lien 1fichier trouvé", OUTPUT_JSON)
         process.exit(1)
     }
 
@@ -184,14 +161,14 @@ const get_1fichier_page = async (password, token) => {
 const main = async () => {
 
     if (!validate_url(MAIN_URL_EXTRACT)) {
-        display_errors("URL non valide")
+        display_errors("URL non valide", OUTPUT_JSON)
         process.exit(1)
     }
 
     const token = await login()
 
     if (!token) {
-        display_errors("Impossible de récupérer le token")
+        display_errors("Impossible de récupérer le token", OUTPUT_JSON)
         process.exit(1)
     }
 
@@ -200,7 +177,7 @@ const main = async () => {
     if (!OUTPUT_JSON) console.log("Mot de passe:", password_clean)
 
     if (!password_clean) {
-        display_errors("Impossible de récupérer le mot de passe")
+        display_errors("Impossible de récupérer le mot de passe", OUTPUT_JSON)
         process.exit(1)
     }
 
